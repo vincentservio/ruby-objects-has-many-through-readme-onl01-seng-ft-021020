89ad56009a8e578d6ecc070318e0b01c18ff5971
Vincent Servio <vincentservio@gmail.com>
2020-02-25 22:21:08 +0000

    Done.

--- lib/meal.rb
+++ lib/meal.rb
@@ -1,3 +1,17 @@
 class Meal
+    attr_accessor :customer, :waiter, :total, :tip
 
+    @@all = Array.new
+
+    def initialize(waiter, customer, total, tip = 0)
+        @waiter = waiter
+        @customer = customer
+        @total = total
+        @tip = tip
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
 end
--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,3 +1,42 @@
 class Waiter
+    attr_accessor :name, :yrs_experience
 
+    @@all = Array.new
+
+    def initialize(name, yrs_experience)
+        @name = name
+        @yrs_experience = yrs_experience
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def new_meal(customer, total, tip = 0)
+        Meal.new(self, customer, total, tip)
+    end
+
+    def meals
+        Meal.all.select do |meal|
+            meal.waiter == self
+        end
+    end
+
+    def customers
+        meals.map do |meal|
+            meal.customer
+        end.map do |customer|
+            customer.name
+        end.uniq
+    end
+
+  def best_tipper
+  best_tipped_meal = meals.max do |meal_a, meal_b|
+    meal_a.tip <=> meal_b.tip
+  end
+ 
+  best_tipped_meal.customer
+end
+   
 end
